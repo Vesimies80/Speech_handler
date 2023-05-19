46fefb1ca1c7564a72042fe60293ed38ed613285
Samuel Schmidt <sam@theschmidts.eu>
2023-05-19 03:38:28 +0300

    Made dataloader work sadly pickling is not working

--- MFCC_feature_extraction.py
+++ MFCC_feature_extraction.py
@@ -2,12 +2,12 @@ import os
 import librosa
 import numpy as np
 import csv
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.utils import pad_sequences
 
 #A bit manual but good enough for this
-folder_path = 'MELD_MP3/dev'
+folder_path = 'MELD_MP3/test'
 
-output_file = 'MELD_MP3/MELD_MFCC_features_dev.csv'
+output_file = 'MELD_MP3/MELD_MFCC_features_test.csv'
 
 # Parameters for MFCC extraction
 n_mfcc = 13
@@ -23,9 +23,9 @@ all_features = []
 # Loop through each file in the folder
 with open(output_file, 'w', newline='') as csvfile:
     counter = 0
-    writer = csv.writer(csvfile)
+    #writer = csv.writer(csvfile)
     #Header row
-    writer.writerow(['Audiofile', 'MFCC1','MFCC2','MFCC3','MFCC4','MFCC5','MFCC6', 'MFCC7', 'MFCC8', 'MFCC9', 'MFCC10', 'MFCC11', 'MFCC12', 'MFCC13'])
+    #writer.writerow(['Audiofile', 'MFCC1','MFCC2','MFCC3','MFCC4','MFCC5','MFCC6', 'MFCC7', 'MFCC8', 'MFCC9', 'MFCC10', 'MFCC11', 'MFCC12', 'MFCC13'])
     for file_name in os.listdir(folder_path):
         counter += 1
         if file_name.endswith('.mp3'):
@@ -40,12 +40,17 @@ with open(output_file, 'w', newline='') as csvfile:
             mfcc_features_normalized = librosa.util.normalize(mfcc_features)
             
             mfcc_features_normalized = pad_sequences(mfcc_features_normalized, maxlen = max_length, padding='post', truncating='post', dtype='float32')
+            audio_name = file_name.replace(".mp3", "")
+            csvfile.write(audio_name + '::')
+            for i in range(13):
+                if i < 12:
+                    csvfile.write(np.array2string(mfcc_features_normalized[i], separator=',', max_line_width=10000000000000000) + '::')
+                else:
+                    csvfile.write(np.array2string(mfcc_features_normalized[i], separator=',', max_line_width=10000000000000000) + '\n')
             
-            
-            
-            writer.writerow([file_name, mfcc_features_normalized[0], mfcc_features_normalized[1], mfcc_features_normalized[2], mfcc_features_normalized[3], 
-                             mfcc_features_normalized[4], mfcc_features_normalized[5], mfcc_features_normalized[6], mfcc_features_normalized[7], mfcc_features_normalized[8],
-                             mfcc_features_normalized[9], mfcc_features_normalized[10], mfcc_features_normalized[11], mfcc_features_normalized[12]])
+            #writer.writerow([file_name, mfcc_features_normalized[0], mfcc_features_normalized[1], mfcc_features_normalized[2], mfcc_features_normalized[3], 
+            #                 mfcc_features_normalized[4], mfcc_features_normalized[5], mfcc_features_normalized[6], mfcc_features_normalized[7], mfcc_features_normalized[8],
+            #                 mfcc_features_normalized[9], mfcc_features_normalized[10], mfcc_features_normalized[11], mfcc_features_normalized[12]])
             # Append the features to the list
             all_features.append(mfcc_features_normalized)
             if counter % 100 == 0:
---
+++ dataloader.py
@@ -0,0 +1,161 @@
+import torch
+from transformers import AutoTokenizer, AutoModel
+import pandas as pd
+
+from csv import DictReader
+
+class Dataloader:
+    #funny enough the longest single utterance in training is 69 words long
+    #TODO consider is there a better max_length and why
+    def __init__(self, mode, max_length=69, tokenizer=None, nmfcc=13) -> None:
+    
+        if tokenizer is not None:
+            self.tokenizer = tokenizer
+            
+        else:
+            self.tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+        self.nmfcc = nmfcc
+        self.max_length = max_length
+        self.mode = mode
+        self.audio_data = {}
+        # Load csv files for feature extraction
+        if self.mode == "dev":
+            with open("MELD_MP3/dev_sent_emo.csv") as f:
+                
+                dict_read = DictReader(f)
+                self.text_data = list(dict_read)
+            with open('MELD_MP3/MELD_MFCC_features_dev.csv') as file:
+                for line in file:
+                    line = line.replace("[", "")
+                    line = line.replace("]", "")
+                    cleaned_line = line.strip().split('::')
+                    key = cleaned_line[0]
+                    strvalues = cleaned_line[1:]
+                    values = []
+                    for value in strvalues:
+                        str_value = value.split(",")
+                        int_value =[]
+                        for element in str_value:
+                            int_value.append(float(element))
+                        values.append(int_value)
+                    self.audio_data[key] = values
+
+        if self.mode == "test":
+            with open("MELD_MP3/test_sent_emo.csv") as f:
+                
+                dict_read = DictReader(f)
+                self.text_data = list(dict_read)
+            with open('MELD_MP3/MELD_MFCC_features_test.csv') as file:
+                for line in file:
+                    line = line.replace("[", "")
+                    line = line.replace("]", "")
+                    cleaned_line = line.strip().split('::')
+                    key = cleaned_line[0]
+                    strvalues = cleaned_line[1:]
+                    values = []
+                    for value in strvalues:
+                        str_value = value.split(",")
+                        int_value =[]
+                        for element in str_value:
+                            int_value.append(float(element))
+                        values.append(int_value)
+                    self.audio_data[key] = values
+            
+        if self.mode == "train":    
+            with open("MELD_MP3/train_sent_emo.csv") as f:
+                
+                dict_read = DictReader(f)
+                self.text_data = list(dict_read)
+            with open('MELD_MP3/MELD_MFCC_features_train.csv') as file:
+                for line in file:
+                    line = line.replace("[", "")
+                    line = line.replace("]", "")
+                    cleaned_line = line.strip().split('::')
+                    key = cleaned_line[0]
+                    strvalues = cleaned_line[1:]
+                    values = []
+                    for value in strvalues:
+                        str_value = value.split(",")
+                        int_value =[]
+                        for element in str_value:
+                            int_value.append(float(element))
+                        values.append(int_value)
+                    self.audio_data[key] = values
+            
+        self.n_data = len(self.text_data)
+        print(self.n_data)
+        print(self.text_data[0:2])
+        print(self.audio_data["dia0_utt0"][0][0])
+        
+        self.data = {}
+        self.text_only = {}
+        self.pair_audio_and_text()
+        #self.pair_audio_text_and_prepare_data()
+        
+    #Obsolete
+    def parse_csv(self, file_text):
+        dict_read = DictReader(file_text)
+        for row in dict_read:
+            yield row
+    #Obsolete
+    def max_lenght_text(self):
+        max_len = 0
+        for text in self.text_data:
+            lenght = len(text['Utterance'].split(' '))
+            if  lenght > max_len:
+                max_len = lenght
+        return max_len
+    
+    
+    def prepare_text_data(self):
+        for text in self.text_data:
+            dia = text["Dialogue_ID"]
+            ut = text["Utterance_ID"]
+            idx = 'dia'+str(dia) + '_' + 'utt' +str(ut)
+
+            label = text['Emotion']
+            only_text = text['Utterance']
+            
+            tokenized_text = self.tokenizer(only_text, add_special_tokens=True, return_tensors='pt', padding='max_length', truncation=True, max_length=self.max_length)
+            
+            self.text_only[idx] = [label, tokenized_text]
+            
+            
+    def pair_audio_and_text(self):
+        self.prepare_text_data()
+        for key,value in self.audio_data.items():
+            label = self.text_only[key][0]
+            text = self.text_only[key][1]
+            audio_features = value
+            self.data[key] = [text, audio_features, label]
+            
+    #Obsolete
+    def pair_audio_text_and_prepare_data(self):
+        
+        for text in self.text_data:
+            dia = text["Dialogue_ID"]
+            ut = text["Utterance_ID"]
+            idx = str(dia) + '_' + str(ut)
+
+            label = text['Emotion']
+            Sr_no = text['Sr No.']
+            only_text = text['Utterance']
+            
+            tokenized_text = self.tokenizer(only_text, add_special_tokens=True, return_tensors='pt', padding='max_length', truncation=True, max_length=self.max_length)
+            
+            sample = {
+                "tokenized_text": tokenized_text,
+                "audio_features": self.audio_data[idx],
+                "label": label,
+                "Sr_no": Sr_no
+            }
+            self.data.append(sample)
+            
+        return
+    
+    def __len__(self):
+        return self.n_data
+    
+    def __getitem__(self,idx):
+        
+        return self.data[idx]
